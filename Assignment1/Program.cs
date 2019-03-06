349055ed1e611c4c89edc2585f68fbb176539ac3
Gurkirat Singh <c0731779@mylambton.ca>
2019-03-06 08:44:31 -0500

    Final Commit

--- Assignment1/Program.cs
+++ Assignment1/Program.cs
@@ -1,81 +1,140 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Assignment1
+namespace C0731779__ASSIGNMENT_
 {
-    class Program
+    class JournalEntry
     {
-        static void Main(string[] args)
+        public JournalEntry(string note, int dist)
         {
-            Countryside a = new Countryside();
-            a.Data();
+            villageName = note; distanceTraveled = dist;
+            HowFarToGetBack = distanceTraveled;
+        }
+        public int HowFarToGetBack = 0;
+        private string villageName;
+        private int distanceTraveled;
+        public int getDistanceWalked() { return distanceTraveled; }
+        public string getVillageName() { return villageName; }
+    }
+    class Hugi
+    {
+        private static JournalEntry je;
+        public static bool FoundAstrilde = false;
+        public static List<JournalEntry> HugiJournal = new List<JournalEntry>();
+        public static int CalculateDistanceWalked()
+        {
+            int DistanceWalked = 0;
+            foreach (var je in HugiJournal)
+            {
+                Console.WriteLine(" {0}  --   {1} *** --- {2} ", je.getDistanceWalked(), je.getVillageName(), je.HowFarToGetBack);
+                DistanceWalked += je.getDistanceWalked() + je.HowFarToGetBack;
+            }
+            return DistanceWalked;
         }
     }
-    class Countryside
-    {
-        public void Data()
-        {
-            
-            Village Alst = new Village("Alst", false);
-            Village Schvenig = new Village("Schvenig", false);
-            Village Maeland = new Village("Maeland", false);
-            Village Helmholtz = new Village("Helmholtz", false);
-            Village Wessig = new Village("Wesig", false);
-            Village Uster = new Village("Uster", false);
-            Village Badden = new Village("Badden", true);
 
-    }
-    public class Village
+    class CountrySide
     {
-        Village Alst;
-        Village Schvenig;
+        static void Main()
+        {
+            CountrySide a = new CountrySide();
+            a.Run();
+        }
         Village Maeland;
         Village Helmholtz;
+        Village Alst;
         Village Wessig;
-        Village Uster;
         Village Badden;
+        Village Uster;
+        Village Schvenig;
 
-        public Village westVillage;
-        public Village eastVillage;
-        public int DistanceTravelled;
-        public string AstrildeVillage;
-        public bool isHere;
-            public Village(string VillageName, bool AstrildeLocation)
+        public void TraverseVillages(Village CurrentVillage)
+        {
+            if (Hugi.FoundAstrilde) return;
+
+
+            Hugi.HugiJournal.Add(new JournalEntry(CurrentVillage.VillageName, CurrentVillage.distanceFromPreviousVillage));
+            try
             {
-                AstrildeVillage = VillageName;
-                isHere = AstrildeLocation;
-                if (isHere == false)
+                Console.WriteLine("I am in {0}", CurrentVillage.VillageName);
+                if (CurrentVillage.isAstrildgeHere)
                 {
-                    Console.WriteLine("------------------------------------");
-                    Console.WriteLine("Hugi went to **{0}**, He didn't find Astrilde there.", VillageName);
-                    Console.WriteLine("-------------------------------------");
-                    Console.ReadLine();
-
+                    Console.WriteLine("I found Dear Astrildge in {0}", CurrentVillage.VillageName);
+                    Console.WriteLine("**** FEELING HAPPY!!! ******");
+                    Console.WriteLine("Astrilde, I walked {0} vika to find you. Will you marry me?", Hugi.CalculateDistanceWalked());
+                    Hugi.FoundAstrilde = true;
                 }
-                else if (isHere == true)
-                {
-                    Console.WriteLine(" ");
-                    Console.WriteLine("Atlast, Hugi found Astrilde in village named **{0}**", VillageName);
-                    Console.WriteLine("**  **");
-                    Console.ReadLine();
+                TraverseVillages(CurrentVillage.west);
+                TraverseVillages(CurrentVillage.east);
+            }
+            catch (NullReferenceException) { }
+        }
 
-                    Console.WriteLine("*-------------------------------------------*");
-                    Console.WriteLine("An Announcement was posted *The Viking Village Newspaper* later that week: ");
-                    Console.WriteLine(" ");
-                    Console.WriteLine("The Familes of Astridle Guðmundsdóttir and Hugi Ólafur");
-                    Console.WriteLine("are Happy to announce the Marriage of their Children.");
-                    Console.WriteLine(" ");
-                    Console.WriteLine("The Ceremony will take place on the third Sunnudagur of Góa ");
-                    Console.WriteLine("at the Temple of Frigga in Alst.");
-                    Console.WriteLine(" ");
-                    Console.WriteLine("****All are Welcome.*****");
+        public void Run()
+        {
+            Alst = new Village("Alst", false);
+            Schvenig = new Village("Schvenig", false);
+            Wessig = new Village("Wessig", false);
+            Maeland = new Village("Maeland", false);
+            Helmholtz = new Village("helmholtz", false);
+            Uster = new Village("Uster", true);
+            Badden = new Village("Badden", false);
+
+            Alst.VillageSetup(0, Schvenig, Wessig);
+            Schvenig.VillageSetup(14, Maeland, Helmholtz);
+            Maeland.VillageSetup(9, null, Helmholtz);
+            Helmholtz.VillageSetup(28, null, null);
+            Wessig.VillageSetup(19, Uster, Badden);
+            Uster.VillageSetup(28, null, null);
+            Badden.VillageSetup(11, null, null);
+
+            this.TraverseVillages(Alst);
+            this.Announcement();
+        }
+        public void Announcement()
+        {
+            try
+            {
+
+                using (StreamReader sr = new StreamReader("C:/Users/kirat/source/repos/C0731779-(ASSIGNMENT)/announcement.txt"))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
                 }
             }
-            
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
         }
-       
     }
+    class Village
+    {
+        public Village(string _villageName, bool _isAHere)
+        {
+            isAstrildgeHere = _isAHere;
+            VillageName = _villageName;
+        }
+        public void VillageSetup(int _prevVillageDist, Village _westVillage, Village _eastVillage)
+        {
+            east = _eastVillage;
+            west = _westVillage;
+            distanceFromPreviousVillage = _prevVillageDist;
+        }
+        public Village west;
+        public Village east;
+        public string VillageName;
+        public int distanceFromPreviousVillage;
+        public bool isAstrildgeHere;
+    }
+
 }
+
